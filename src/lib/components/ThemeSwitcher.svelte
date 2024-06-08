812e59974012c73f4161e780aebcae7a7191cad4
beeb <703631+beeb@users.noreply.github.com>
2024-06-08 17:29:55 +0200

    refactor: default to dark theme

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -3,41 +3,40 @@
   import Sun from "virtual:icons/mingcute/sun-fill";
   import Moon from "virtual:icons/mingcute/moon-fill";
 
-  const KEY_NAME = "dark-theme-enabled";
+  const KEY_NAME = "light-theme-enabled";
 
-  let dark = false;
+  let light = false;
 
   onMount(() => {
-    let value = localStorage.getItem(KEY_NAME);
+    const value = localStorage.getItem(KEY_NAME);
     if (value) {
       const checked = JSON.parse(value);
       if (checked === true) {
-        dark = true;
+        light = true;
       }
-    } else {
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        dark = true;
+    } else if (window.matchMedia) {
+      if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        light = true;
         localStorage.setItem(KEY_NAME, JSON.stringify(true));
+      } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        localStorage.setItem(KEY_NAME, JSON.stringify(false));
       }
     }
   });
   function handleChange() {
-    localStorage.setItem(KEY_NAME, JSON.stringify(dark));
+    localStorage.setItem(KEY_NAME, JSON.stringify(light));
   }
 </script>
 
 <label class="flex items-center cursor-pointer gap-2">
-  <Sun />
+  <Moon />
   <input
     type="checkbox"
-    value="dark"
-    bind:checked={dark}
+    value="light"
+    bind:checked={light}
     on:change={handleChange}
     class="toggle theme-controller"
     id="theme-controller"
   />
-  <Moon />
+  <Sun />
 </label>
