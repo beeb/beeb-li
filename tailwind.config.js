068ccdd36f46e3ebb3b9a9661637a5cc20bd41d6
beeb <703631+beeb@users.noreply.github.com>
2024-06-08 18:04:09 +0200

    feat: custom monospace font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts,md}'],
 	theme: {
+		fontFamily: {
+			mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+		},
 		extend: {},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
