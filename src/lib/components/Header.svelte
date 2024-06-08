1998e43949a33cb04daf211fe955acca54d15494
beeb <703631+beeb@users.noreply.github.com>
2024-06-08 09:26:51 +0200

    feat: better navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Nav from "./Nav.svelte";
   import { siteTitle } from "$lib/config";
+  import Hamburger from "virtual:icons/mingcute/menu-line";
 
   const focusMain = () => {
     const main = document.querySelector("main");
@@ -18,17 +19,18 @@
   </a>
 </div>
 <header class="navbar mb-8">
-  <div class="navbar-start">
+  <div class="navbar-start gap-2 w-full sm:w-1/3">
     <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost sm:hidden">HB</div>
+      <div tabindex="0" role="button" class="btn btn-ghost sm:hidden">
+        <Hamburger />
+      </div>
       <Nav
-        cl="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        cl="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
       />
     </div>
-    <a href="/" class="btn btn-ghost text-2xl">{siteTitle}</a>
+    <a href="/" class="text-2xl text-primary font-bold">{siteTitle}</a>
   </div>
-  <div class="navbar-center hidden sm:flex">
+  <div class="navbar-end hidden sm:flex w-2/3">
     <Nav cl="menu menu-lg menu-horizontal px-2 gap-2" />
   </div>
-  <div class="navbar-end"></div>
 </header>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
 
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+		}),
 	],
 	server: {
 		fs: {
-			allow: ['.']
-		}
-	}
+			allow: ['.'],
+		},
+	},
 }
 
 export default config
