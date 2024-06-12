8cccce81321d9bd45a5fe9f8d57f1cb4111555f1
beeb <703631+beeb@users.noreply.github.com>
2024-06-12 19:30:04 +0200

    chore: skeleton for custom vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import type { UserConfig } from 'vite'
+import type { UserConfig, Plugin } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import arraybuffer from 'vite-plugin-arraybuffer'
+import { imagetools, type VitePluginOptions } from 'vite-imagetools'
 import Icons from 'unplugin-icons/vite'
 
 const config: UserConfig = {
@@ -9,6 +10,7 @@ const config: UserConfig = {
 		enhancedImages(),
 		sveltekit(),
 		arraybuffer(),
+		base64Image(),
 		Icons({
 			compiler: 'svelte',
 		}),
@@ -18,6 +20,40 @@ const config: UserConfig = {
 			allow: ['.'],
 		},
 	},
+}
+
+function base64Image(): Plugin[] {
+	const imagetoolsInstance = imagetoolsPlugin()
+	return [base64Plugin(imagetoolsInstance), imagetoolsInstance]
+}
+
+function imagetoolsPlugin() {
+	const imagetoolsOpts: Partial<VitePluginOptions> = {
+		defaultDirectives: async ({ pathname, searchParams: qs }, metadata) => {
+			if (!qs.has('base64image')) return new URLSearchParams()
+
+			const imgWidth = qs.get('width')
+			const width = imgWidth ? Number.parseInt(imgWidth) : (await metadata()).width
+			if (!width) {
+				console.warn(`Could not determine width of image ${pathname}`)
+				return new URLSearchParams()
+			}
+
+			return new URLSearchParams({
+				format: 'jpg',
+				w: `${width}`,
+			})
+		},
+		namedExports: false,
+	}
+
+	return imagetools(imagetoolsOpts)
+}
+
+function base64Plugin(imagetoolsPlugin: Plugin): Plugin {
+	return {
+		name: 'vite-plugin-base64-image',
+	}
 }
 
 export default config
