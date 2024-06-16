f8dc1acf83aed379f8cb07a9c873061f52a2a357
beeb <703631+beeb@users.noreply.github.com>
2024-06-16 19:12:55 +0200

    fix: dropdown behavior on Safari

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-  import Nav from "$lib/components/Nav.svelte";
-  import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
-  import Logo from "$lib/components/Logo.svelte";
-  import { siteTitle } from "$lib/config";
-  import Hamburger from "virtual:icons/mingcute/menu-fill";
+  import Nav from '$lib/components/Nav.svelte'
+  import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte'
+  import Logo from '$lib/components/Logo.svelte'
+  import { siteTitle } from '$lib/config'
+  import Hamburger from 'virtual:icons/mingcute/menu-fill'
 
   const focusMain = () => {
-    const main = document.querySelector("main");
-    main?.focus();
-  };
+    const main = document.querySelector('main')
+    main?.focus()
+  }
 </script>
 
 <div class="text-center">
-  <a
-    on:click|preventDefault={focusMain}
-    href="#main"
-    class="link absolute left-[-1000%] top-[-1000%] focus:static"
-  >
+  <a on:click|preventDefault={focusMain} href="#main" class="link absolute left-[-1000%] top-[-1000%] focus:static">
     Skip to main content
   </a>
 </div>
@@ -26,14 +22,9 @@
       <div tabindex="0" role="button" class="btn btn-ghost text-lg">
         <Hamburger />
       </div>
-      <Nav
-        class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-      />
+      <Nav tabindex="0" class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52" />
     </div>
-    <a
-      href="/"
-      class="text-2xl sm:text-3xl text-primary font-extrabold inline-flex gap-2 items-center"
-    >
+    <a href="/" class="text-2xl sm:text-3xl text-primary font-extrabold inline-flex gap-2 items-center">
       <Logo class="w-8 sm:w-10 shrink-0" />
       {siteTitle}
     </a>
