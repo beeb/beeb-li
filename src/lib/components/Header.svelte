44664520d5189795d12299225dffec5f0b8f05f6
beeb <703631+beeb@users.noreply.github.com>
2025-04-11 08:20:22 +0200

    feat(header): improve accessibility

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,32 +1,47 @@
 <script lang="ts">
-  import type { MouseEventHandler } from 'svelte/elements'
-  import Nav from '$lib/components/Nav.svelte'
-  import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte'
-  import Logo from '$lib/components/Logo.svelte'
-  import { siteTitle } from '$lib/config'
-  import Hamburger from 'virtual:icons/mingcute/menu-fill'
+  import type { MouseEventHandler } from "svelte/elements";
+  import Nav from "$lib/components/Nav.svelte";
+  import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
+  import Logo from "$lib/components/Logo.svelte";
+  import { siteTitle } from "$lib/config";
+  import Hamburger from "virtual:icons/mingcute/menu-fill";
 
   const focusMain: MouseEventHandler<HTMLAnchorElement> = (e) => {
-    e.preventDefault()
-    const main = document.querySelector('main')
-    main?.focus()
-  }
+    e.preventDefault();
+    const main = document.querySelector("main");
+    main?.focus();
+  };
 </script>
 
 <div class="text-center">
-  <a onclick={focusMain} href="#main" class="link absolute left-[-1000%] top-[-1000%] focus:static">
+  <a
+    onclick={focusMain}
+    href="#main"
+    class="link absolute left-[-1000%] top-[-1000%] focus:static"
+  >
     Skip to main content
   </a>
 </div>
 <header class="navbar mb-8 px-0 min-h-20">
   <div class="navbar-start grow w-auto">
     <div class="dropdown sm:hidden relative -left-4">
-      <div tabindex="0" role="button" class="btn btn-ghost text-lg">
+      <div
+        tabindex="0"
+        role="button"
+        class="btn btn-ghost text-lg"
+        aria-label="Open dropdown menu"
+      >
         <Hamburger />
       </div>
-      <Nav tabindex="0" class="menu menu-lg dropdown-content mt-3 z-1 p-2 shadow-sm bg-base-200 rounded-box w-52" />
+      <Nav
+        tabindex="0"
+        class="menu menu-lg dropdown-content mt-3 z-1 p-2 shadow-sm bg-base-200 rounded-box w-52"
+      />
     </div>
-    <a href="/" class="text-2xl sm:text-3xl text-primary font-extrabold inline-flex gap-2 items-center">
+    <a
+      href="/"
+      class="text-2xl sm:text-3xl text-primary font-extrabold inline-flex gap-2 items-center"
+    >
       <Logo class="w-8 sm:w-10 shrink-0" />
       {siteTitle}
     </a>
