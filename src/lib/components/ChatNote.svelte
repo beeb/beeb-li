7b5404b9e2892eb9210481be1928bb5e40a6788c
beeb <703631+beeb@users.noreply.github.com>
2025-04-11 09:03:21 +0200

    perf: optimize cartoon png

--- src/lib/components/ChatNote.svelte
+++ src/lib/components/ChatNote.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-  import type { Snippet } from 'svelte'
+  import type { Snippet } from "svelte";
+  import cartoon from "$lib/assets/beeb_cartoon.png?enhanced";
 
   interface Props {
-    children: Snippet
+    children: Snippet;
   }
 
-  const { children }: Props = $props()
+  const { children }: Props = $props();
 </script>
 
 <div class="chat chat-end">
   <div class="chat-image avatar">
     <div class="w-24">
-      <img class="m-0" alt="Tailwind CSS chat bubble component" src="/beeb_cartoon.png" />
+      <enhanced:img
+        class="m-0"
+        src={cartoon}
+        alt="A cartoon portrait of the author"
+      ></enhanced:img>
     </div>
   </div>
   <div class="chat-bubble rounded-box">
