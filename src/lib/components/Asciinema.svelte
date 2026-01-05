37f40f64ed9b8d46f88cc203882384f3bbbf1847
beeb <703631+beeb@users.noreply.github.com>
2026-01-05 08:32:55 +0100

    feat: theme

--- src/lib/components/Asciinema.svelte
+++ src/lib/components/Asciinema.svelte
@@ -16,6 +16,7 @@
     // Dynamically import asciinema-player only in the browser
     import('asciinema-player').then((AsciinemaPlayer) => {
       AsciinemaPlayer.create(url, element as HTMLElement, {
+        theme: 'catppuccin',
         idleTimeLimit: 0.5,
         loop,
         cols,
@@ -29,7 +30,30 @@
 {#if fallback}
   <noscript>
     <a href={fallback} target="_blank" title="Watch cast on asciinema.org">
-      <img src={`${fallback}.svg`} alt="A still image from an Asciinema terminal screencast" />
+      <img src={`${fallback}.svg`} alt="A still from an Asciinema terminal screencast" />
     </a>
   </noscript>
 {/if}
+
+<style>
+  :global(.asciinema-player-theme-catppuccin) {
+    --term-color-foreground: #cdd6f4;
+    --term-color-background: #1e1e2e;
+    --term-color-0: #45475a;
+    --term-color-1: #f38ba8;
+    --term-color-2: #a6e3a1;
+    --term-color-3: #f9e2af;
+    --term-color-4: #89b4fa;
+    --term-color-5: #f5c2e7;
+    --term-color-6: #94e2d5;
+    --term-color-7: #bac2de;
+    --term-color-8: #585b70;
+    --term-color-9: #f38ba8;
+    --term-color-10: #a6e3a1;
+    --term-color-11: #f9e2af;
+    --term-color-12: #89b4fa;
+    --term-color-13: #f5c2e7;
+    --term-color-14: #94e2d5;
+    --term-color-15: #a6adc8;
+  }
+</style>
