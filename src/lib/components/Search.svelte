bd801de99a5dff9644058b70d9dbd15f7f709a83
beeb <703631+beeb@users.noreply.github.com>
2026-01-24 10:21:39 +0100

    fix: alignment

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { Attachment } from 'svelte/attachments'
   import { onNavigate } from '$app/navigation'
   import SearchIcon from 'virtual:icons/mingcute/search-3-fill'
 
@@ -17,6 +18,7 @@
     return []
   }
 
+  let showField = $state(false)
   let searchValue = $state('')
   let searchResults = $derived(fetchSearchResults(searchValue))
 
@@ -29,35 +31,55 @@
       pagefind.init()
     })()
   })
+
+  // unhide if JS is enabled
+  const unhideAttachment: Attachment = (element) => {
+    ;(element as HTMLElement).classList.remove('hidden')
+  }
 </script>
 
-<div class="md:hidden">
-  <button type="button" class="btn btn-ghost">
-    <SearchIcon />
-  </button>
-</div>
-<div class="dropdown dropdown-open dropdown-end hidden md:block">
-  <label class="input input-bordered w-36 lg:w-40">
-    <SearchIcon />
-    <input type="search" placeholder="Search" bind:value={searchValue} />
-  </label>
-  {#if searchValue}
-    <div class="dropdown-content bg-base-100 rounded-box p-4 mt-2 z-1 w-100 m-w-9/10 shadow-md">
-      {#await searchResults}
-        <div>loading...</div>
-      {:then results}
-        {#each results as result, i (result.url)}
-          <a href={result.url.replace('.html', '')} class="block">
-            <div class="flex flex-col gap-1">
-              <h3>{result.meta.title}</h3>
-              <div class="text-xs wrap-break-word">{@html result.excerpt}</div>
-            </div>
-          </a>
-          {#if i < results.length - 1}
-            <div class="divider"></div>
-          {/if}
-        {/each}
-      {/await}
+<div class="hidden" {@attach unhideAttachment}>
+  <div class="md:hidden">
+    <button
+      type="button"
+      class="btn btn-ghost"
+      onclick={() => {
+        if (showField) {
+          searchValue = ''
+        }
+        showField = !showField
+      }}
+    >
+      <SearchIcon />
+    </button>
+  </div>
+  <div class="absolute top-12 right-0 md:static md:top-0">
+    <div class="dropdown dropdown-open dropdown-end md:block" class:hidden={!showField && searchValue === ''}>
+      <label class="input input-bordered w-40 md:w-36 lg:w-40">
+        <SearchIcon />
+        <input type="search" placeholder="Search" bind:value={searchValue} />
+      </label>
+      {#if searchValue}
+        <div
+          class="dropdown-content bg-base-100 rounded-box p-4 mt-2 z-1 w-100 md:w-120 lg:w-160 max-w-[90vw] shadow-md"
+        >
+          {#await searchResults}
+            <div>loading...</div>
+          {:then results}
+            {#each results as result, i (result.url)}
+              <a href={result.url.replace('.html', '')} class="block">
+                <div class="flex flex-col gap-1">
+                  <h3>{result.meta.title}</h3>
+                  <div class="text-xs wrap-break-word">{@html result.excerpt}</div>
+                </div>
+              </a>
+              {#if i < results.length - 1}
+                <div class="divider"></div>
+              {/if}
+            {/each}
+          {/await}
+        </div>
+      {/if}
     </div>
-  {/if}
+  </div>
 </div>
