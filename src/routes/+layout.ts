0bf1b6495fbf281ac860645a722c932215f7d76e
beeb <703631+beeb@users.noreply.github.com>
2026-02-26 07:54:54 +0100

    fix: ssg

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import type { LayoutLoad } from './$types'
 
 // Ensures all pages under this layout (which is all of them) are statically prerendered at build time
 export const prerender = true
+export const trailingSlash = 'always'
 
 export const csr = true
 
